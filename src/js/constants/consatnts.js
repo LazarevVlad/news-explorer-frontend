2a2a5b6aec25052044f5b72d59726e1f1c02c4f1
Vladislav Lazarev <lazarevvladv@yandex.ru>
2020-09-13 01:46:18 +0700

    added constants

---
+++ src/js/constants/consatnts.js
@@ -0,0 +1,7 @@
+const option = {
+  newsApiUrl: 'http://newsapi.org/v2/everything?',
+  mainApiUrl: 'https://api.findnews.gq',
+  newsKey: 'da16eee876a84da689fb591539326b29',
+};
+
+export default option;
